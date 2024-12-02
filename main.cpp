bd67618f9d474e9f52763b5da0f03a97d6811483
Sherif Gabr <sgabr21@student.oulu.if>
2024-12-02 03:25:08 +0200

    Draw triangles

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 ﻿#include "tgaimage.h"
-#include "model.h"
 #include "geometry.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -9,29 +9,29 @@ const int width = 800;
 const int height = 800;
 Model* model = NULL;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color)
 {
 	bool steep = false;
 
 	// checks if the slope is steep and swaps xs and ys to make it less steep
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-		std::swap(x0, y0);
-		std::swap(x1, y1);
+	if (std::abs(t0.x - t1.x) < std::abs(t0.y - t1.y)) {
+		std::swap(t0.x, t0.y);
+		std::swap(t1.x, t1.y);
 		steep = true;
 	}
 	// needed to make x0 smaller than x1
-	if (x0 > x1) {
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+	if (t0.x > t1.x) {
+		std::swap(t0.x, t1.x);
+		std::swap(t0.y, t1.y);
 	}
 
-	int dx = x1 - x0;
-	int dy = y1 - y0;
+	int dx = t1.x - t0.x;
+	int dy = t1.y - t0.y;
 	float derror2 = std::abs(dy) * 2;
 	float error2 = 0;
-	int y = y0;
+	int y = t0.y;
 
-	for (int x = x0; x <= x1; x++) {
+	for (int x = t0.x; x <= t1.x; x++) {
 		if (steep)
 			image.set(y, x, color);
 		else
@@ -40,8 +40,34 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 		error2 += derror2;
 		
 		if (error2 > dx) {
-			y += y1 > y0? 1 : -1;
+			y += t1.y > t0.y? 1 : -1;
 			error2 -= 2*dx;
+		}
+	}
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+{
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+
+	int triangle_height = t2.y - t0.y;
+	
+	for (int i = 0; i < triangle_height; ++i) {
+		bool upper_segment = i > (t1.y - t0.y) || t1.y == t0.y;
+		int segment_height = upper_segment ? t2.y - t1.y : t1.y - t0.y;
+
+		float alpha = (float) i / triangle_height;
+		float beta = (float) (i - (upper_segment ? (t1.y - t0.y) : 0 )) / segment_height;
+
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = upper_segment ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+		if (A.x > B.x) std::swap(A, B);
+		
+		for (int j = A.x; j <= B.x; ++j) {
+			image.set(j, t0.y + i, color);			
 		}
 	}
 }
@@ -50,20 +76,14 @@ int main(int argc, char** argv) {
 	model = new Model("african_head.obj");
 		
 	TGAImage image(width, height, TGAImage::RGB);
-	for (int i = 0; i < model->nfaces(); i++) {
-		std::vector<int> face = model->face(i);
-		for (int j = 0; j < 3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-		
-			line(x0, y0, x1, y1, image, white);
-		}
-	}
+	
+	// rendering triangles
+	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+	triangle(t0[0], t0[1], t0[2], image, red);
+	triangle(t1[0], t1[1], t1[2], image, white);
+	triangle(t2[0], t2[1], t2[2], image, green);
 
 	image.flip_vertically(); 
 	image.write_tga_file("output.tga");
